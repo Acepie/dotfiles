a9cab78180f6d5a60e8d264f995e822b96a06f84
Ameen Radwan <ameen_radwan@intuit.com>
2025-08-05 17:27:21 -0400

    font fix and keymaps

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -4,34 +4,24 @@ return {
   cmd = 'Trouble',
   keys = {
     {
-      '<leader>xx',
+      '<leader>td',
       '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
+      desc = '[T]oggle [D]iagnostics window',
     },
     {
-      '<leader>xX',
+      '<leader>tbd',
       '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-      desc = 'Buffer Diagnostics (Trouble)',
+      desc = '[T]oggle [B]uffer [D]iagnostics window',
     },
     {
-      '<leader>cs',
+      '<leader>ts',
       '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = 'Symbols (Trouble)',
+      desc = '[T]oggle [S]ymbols window',
     },
     {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-      '<leader>xL',
-      '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-    },
-    {
-      '<leader>xQ',
+      '<leader>tq',
       '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
+      desc = '[T]oggle [Q]uickfix window',
     },
   },
 }
